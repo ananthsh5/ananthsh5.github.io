8381ef2951bd372a15714e0a4fd5917e1e128a36
ananth <ananth5@yahoo.com>
2023-10-22 19:22:13 +0800

    Modfied backend url and css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Todo {
+  ID: number;
+  Title: string;
+  Completed: boolean;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myApp';
+  title = 'Todo-app';
+  todos: Todo[] = [];
+  newTodo: Todo = { ID: 0, Title: '', Completed: false };
+
+  constructor(private http: HttpClient) {
+    this.refreshTodos();
+  }
+
+  refreshTodos() {
+    this.http.get<Todo[]>('http://env-0445775.sh2.hidora.net:8080/todos').subscribe((data) => {
+      this.todos = data;
+	    console.log("refreshTodos:", data);
+    });
+  }
+
+  addTodo() {
+	if (this.newTodo.Title.trim() === '') {
+    // Add appropriate error handling for empty title
+	console.error("Title cannot be empty.");
+	alert("Title cannot be empty. Please enter a valid title.");
+    return;
+    }  
+    this.http.post<Todo>('http://env-0445775.sh2.hidora.net:8080/todos', this.newTodo).subscribe(() => {
+      this.newTodo = { ID: 0, Title: '', Completed: false };
+      this.refreshTodos();
+    });
+  }
+
+  updateTodo(todo: Todo) {
+  this.http.put<Todo>(`http://env-0445775.sh2.hidora.net:8080/todos/${todo.ID}`, todo).subscribe(
+    (data) => {
+      console.log("Updated Todo:", data);
+      this.refreshTodos();
+    },
+    (error) => {
+      console.error("Error updating todo:", error);
+    });
+  }
+  
+  deleteTodo(id: number) {
+    this.http.delete(`http://env-0445775.sh2.hidora.net:8080/todos/${id}`).subscribe(() => {
+      this.refreshTodos();
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,7 +11,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
+	HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
